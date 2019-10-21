ff4c69e3bcd50639cc7920d03078e4702caa96d0
WataruSuzuki <wataru0406@gmail.com>
2019-10-21 22:10:00 +0900

    Modified build script

--- build_then_upload.sh
+++ build_then_upload.sh
@@ -5,7 +5,7 @@ zip GitHubReleasesDrafter.zip GitHubReleasesDrafter
 cd -
 mv .build/release/GitHubReleasesDrafter.zip ./
 
-.build/debug/GitHubReleasesDrafter $1 \
+.build/release/GitHubReleasesDrafter $1 \
     --path "WataruSuzuki/GitHub-Releases-Drafter" \
     --token $PUBLISH_TOKEN \
     --assetFileName "GitHubReleasesDrafter.zip"
